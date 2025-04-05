76d46aca3f1f178bb549c696465d4ccdbd91f382
Mk-7429j <manipkm0004@gmail.com>
2025-04-05 13:54:45 +0530

    after

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -10,6 +10,7 @@ const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
 
+  console.log
   return (
     <nav className="shadow-md py-4 w-full h-auto bg-[#425DAC] flex flex-row items-center justify-between px-[8vw] sticky top-0 z-30">
       <div>
