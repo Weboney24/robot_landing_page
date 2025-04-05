1d22b9379c4d3f56d61663ec0174b8a0b0f81ca2
Mk-7429j <manipkm0004@gmail.com>
2025-04-05 10:05:56 +0530

    new

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,7 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavMenu } from "../helper/dataHelper";
+import { Link, useLocation } from "react-router-dom";
+import { Image_Helper } from "../helper/imageHelper";
+import { ICON_HELPER } from "../helper/iconHelper";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+
+  const location = useLocation();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
+
+  return (
+    <nav className="shadow-md py-4 w-full h-auto bg-[#425DAC] flex flex-row items-center justify-between px-[8vw] sticky top-0 z-30">
+      <div>
+        <Link to={"/"}>
+          <img
+            src={Image_Helper.navlogo}
+            alt="Logo"
+            className="w-[100px] h-auto bg-white"
+          />
+        </Link>
+      </div>
+
+      <div className="hidden lg:flex gap-x-5 xl:gap-x-10 justify-center px-4">
+        <ul className="flex space-x-10">
+          {NavMenu.map((item) => {
+            const isActive = location.pathname === item.path;
+            return (
+              <li key={item.id} className="text-white font-semibold text-lg">
+                <Link
+                  to={item.path}
+                  className={`${
+                    isActive ? "text-[#54B685]" : "hover:text-[#54B685]"
+                  } transition-all duration-300`}
+                >
+                  {item.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="hidden lg:block">
+        <button className="bg-[#54B685] text-white font-semibold rounded py-2 px-4">
+          Get Started!
+        </button>
+      </div>
+
+      <div className="lg:hidden flex items-center">
+        <button onClick={toggleMobileMenu} className="text-white">
+          {isMobileMenuOpen ? (
+            <div>
+              <ICON_HELPER.MENU_CLOSE_ICON className="size-[35px]"/>
+            </div>
+          ) : (
+            <div>
+              <ICON_HELPER.MENU_ICON className="size-[35px]"/>
+            </div>
+          )}
+        </button>
+      </div>
+
+      {isMobileMenuOpen && (
+        <div className="absolute top-[73px] right-0 w-full bg-[#425DAC] lg:hidden px-[5vw] py-4 ">
+          <ul className="space-y-4">
+            {NavMenu.map((item) => {
+              const isActive = location.pathname === item.path;
+              return (
+                <li key={item.id} className="text-white font-semibold text-lg">
+                  <Link
+                    to={item.path}
+                    className={`${
+                      isActive ? "text-[#54B685]" : "hover:text-[#54B685]"
+                    } transition-all duration-300`}
+                  >
+                    {item.name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+          <button className="bg-[#54B685] text-white font-semibold rounded py-2 px-4 w-full mt-4">
+            Get Started!
+          </button>
+        </div>
+      )}
+    </nav>
+  );
 };
 
 export default Navbar;
