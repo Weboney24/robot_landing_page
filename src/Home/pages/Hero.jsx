71715a9b7672a173e736ef9163d6a1e3c6d008ee
Madhanraj <T@MADHAN.com>
2025-04-18 15:11:34 +0530

    emaill add

--- src/Home/pages/Hero.jsx
+++ src/Home/pages/Hero.jsx
@@ -62,9 +62,8 @@ const Hero = () => {
             <div className="w-full h-screen px-[8vw] py-10 flex items-center">
               <div className="w-full min-h-[500px] flex flex-col lg:flex-row items-center justify-between gap-10">
                 <div className="w-full lg:w-[50%] h-full flex flex-col items-start gap-y-10">
-                  <h1 className="text-white md:text-6xl text-xl font-bold capitalize">Empowering Minds with Robotics Education</h1>
-                  <p className="text-white text-xl font-bold"> Dive into the exciting world of AI-powered robotics, where creativity meets cutting-edge technology. These courses are crafted to give you practical skills in building smart robots that think, sense, and move on their own</p>
-
+                  <h1 className="text-white text-xl sm:text-2xl md:text-4xl lg:text-5xl font-bold capitalize">Empowering Mind with Robotic Education</h1>
+                  <p className="text-white text-base sm:text-sm md:text-lg lg:text-xl font-bold">Dive into the world of AI-powered robotics, where creativity meets technology. These courses give you practical skills to build smart robots.</p>
                   <div className="flex flex-col md:flex-row items-center gap-10">
                     <a href="#contact">
                       <button className="font-bold text-xl px-6 py-3 rounded bg-primary text-white">Get Started!</button>
@@ -96,8 +95,8 @@ const Hero = () => {
                   <div className="absolute -top-20 -right-36">
                     <img src={Image_Helper.minibanner4} alt="Mini Banner 1" className="w-[80px] h-auto animate-pulse" />
                   </div>
-                  <h1 className="text-white md:text-6xl text-xl font-bold capitalize">Revolutionize Learning with Robotics Courses</h1>
-                  <p className="text-white text-xl font-bold">These courses are tailored to help you design, develop, and control intelligent robotic systems that can perceive, learn, and adapt in real-world environments.</p>
+                  <h1 className="text-white text-xl sm:text-2xl md:text-3xl lg:text-5xl font-bold capitalize">Revolutionize Learning with Robotics Courses</h1>
+                  <p className="text-white text-base sm:text-sm md:text-lg lg:text-xl font-bold">These courses are tailored to help you design, develop, and control intelligent robotic systems that can perceive, learn, and adapt in real-world environments.</p>
 
                   <div className="flex flex-col md:flex-row items-center gap-10">
                     <a href="#contact">
@@ -125,7 +124,7 @@ const Hero = () => {
           </SwiperSlide>
         </Swiper>
       </div>
-      <div className="w-full md:flex hidden justify-center -mt-20 relative z-20  ">
+      <div className="w-full md:flex hidden justify-center -mt-18 relative z-20  ">
         <div className="w-[90%] max-w-7xl grid grid-cols-1 sm:grid-cols-3 gap-6">
           {cardData.map((res, index) => (
             <div key={index} className="bg-gray-50 p-6 flex items-center gap-4">
